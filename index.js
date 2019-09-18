03fde72c454d6fdd7b74a74249747c23819fa78d
Seven <belac335@gmail.com>
2019-09-18 16:12:04 +0100

    included the validation function

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // All JS code goes here
+
+function validateLogin() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var errorMessage = document.getElementById('errorMessage');
+    var text;
+
+    errorMessage.style.padding = "10px";
+
+    if (email == "") {
+        text = "Please input your email address.";
+        errorMessage.innerHTML =text;
+        document.getElementById('email').focus();
+        return false;
+    }
+
+    if (email.indexOf("@") == 1) {
+        text = "Please input a valid email address.";
+        errorMessage.innerHTML = text;
+        document.getElementById('email').focus();
+        return false;
+    }
+
+    if (password == "") {
+        text = "Please input your password.";
+        errorMessage.innerHTML = text;
+        document.getElementById('password').focus();
+        return false;
+    }
+    
+    if (password.length < 8 || password.length > 12) {
+        text = "Password should not be less than 8 or greater than 12 characters.";
+        errorMessage.innerHTML = text;
+        document.getElementById('password').focus();
+        return false;
+    }
+
+    alert("Login successful!");
+    return false;
+}
